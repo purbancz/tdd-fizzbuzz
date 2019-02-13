acb5c4477bd2b51d39e23de10ee886efc6f62b08
Piotr Urbanczyk <p.urbanczyk@copernicuscenter.edu.pl>
2019-02-13 17:15:26 +0100

    Refactor

--- src/fizzbuzzmechanics/FizzBuzz.java
+++ src/fizzbuzzmechanics/FizzBuzz.java
@@ -7,12 +7,12 @@ public class FizzBuzz {
 
 
 	public String[] fizzBuzzed() {
-		Stream<String> stringStreamTest = IntStream.rangeClosed(1, 100)
+		Stream<String> fizzBuzzedStringStream = IntStream.rangeClosed(1, 100)
 				.mapToObj(i -> String.valueOf(i))
 				.map(i -> Integer.parseInt(i) % 15 == 0 ? "FizzBuzz" :
 					Integer.parseInt(i) % 5 == 0 ? "Buzz" :
 					Integer.parseInt(i) % 3 == 0 ? "Fizz" : i);
-		return stringStreamTest.toArray(size -> new String[size]);
+		return fizzBuzzedStringStream.toArray(size -> new String[size]);
 	}
 
 	public void printFizzBuzzed() {
